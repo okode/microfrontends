9a18659c6913a08e4d9e70c2474a3b1ff5f4e3e3
Pedro Jorquera <pjorquera@okode.com>
2023-01-18 19:02:29 +0100

    Initial commit

---
+++ apps/microfrontends/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'microfrontends-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'microfrontends';
+}
