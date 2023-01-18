4b6fc1870f4c278c6e9d6c3c909b8dbcf130d8cd
Pedro Jorquera <pjorquera@okode.com>
2023-01-18 19:05:33 +0100

    Added remote login

--- apps/dashboard/src/app/app.routes.ts
+++ apps/dashboard/src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'login',
+    loadChildren: () => import('login/Module').then((m) => m.RemoteEntryModule),
+  },
+  {
     path: '',
     component: NxWelcomeComponent,
   },
---
+++ apps/login/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'microfrontends-login-entry',
+  template: `<microfrontends-nx-welcome></microfrontends-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
