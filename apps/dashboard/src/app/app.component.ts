109cbd6b28a6cdd9944725e6de1d095ede38437d
Pedro Jorquera <pjorquera@okode.com>
2023-01-18 19:53:54 +0100

    Consuming login microfrontend from dashboard application

--- apps/dashboard/src/app/app.component.ts
+++ apps/dashboard/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '@microfrontends/shared/data-access-user';
+import { distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'microfrontends-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  template: `
+    <div class="dashboard-nav">Admin Dashboard</div>
+    <div *ngIf="isLoggedIn$ | async; else signIn">
+      You are authenticated so you can see this content.
+    </div>
+    <ng-template #signIn><router-outlet></router-outlet></ng-template>
+  `,
 })
-export class AppComponent {
-  title = 'dashboard';
+export class AppComponent implements OnInit {
+  isLoggedIn$ = this.userService.isUserLoggedIn$;
+  
+  constructor(private userService: UserService, private router: Router) {}
+
+  ngOnInit() {
+    this.isLoggedIn$
+      .pipe(distinctUntilChanged())
+      .subscribe(async (loggedIn) => {
+        setTimeout(() => {
+          if (!loggedIn) {
+            this.router.navigateByUrl('login');
+          } else {
+            this.router.navigateByUrl('');
+          }
+        });
+      });
+  }
 }
